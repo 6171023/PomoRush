bfa3db1e2630381507136ba7fd0855580c619fff
6171023 <riya6171023@gmail.com>
2024-01-20 12:47:13 +0900

    committing remaining files

---
+++ lib/animation/coin.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class PointFlip extends StatefulWidget {
+  final double points;
+
+  PointFlip({required this.points});
+
+  @override
+  _PointFlipState createState() => _PointFlipState();
+}
+
+class _PointFlipState extends State<PointFlip> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _controller.value * math.pi * 2,
+          child: child,
+        );
+      },
+      child: Image.asset(
+        'assets/coin.png',
+        width: 120, // Increase width here
+        height: 120, // Increase height here
+      ),
+    );
+  }
+}
+
+class CoinFlip extends StatefulWidget {
+  final double money;
+
+  CoinFlip({required this.money});
+
+  @override
+  _CoinFlipState createState() => _CoinFlipState();
+}
+
+class _CoinFlipState extends State<CoinFlip> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _controller.value * math.pi * 2,
+          child: child,
+        );
+      },
+      child: Image.asset(
+        'assets/goen.png',
+        width: 120, // Increase width here
+        height: 120, // Increase height here
+      ),
+    );
+  }
+}
---
+++ lib/screens/moneySettingScreen.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:pomo_rush/utils/preferences.dart';
+import 'package:pomo_rush/widgets/moneytimer.dart';
+
+class MoneySettingsScreen extends StatefulWidget {
+  const MoneySettingsScreen({super.key});
+  @override
+  State<MoneySettingsScreen> createState() => _MoneySettingsScreenState();
+}
+
+class _MoneySettingsScreenState extends State<MoneySettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Styles.pomodoroPrimaryColor,
+        title: Text('Pomodoro Money Timer', style: TextStyle(color: Colors.white)),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white), // Custom back button
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: MoneyTimerWidget(),
+        ),
+      ),
+    );
+  }
+}
